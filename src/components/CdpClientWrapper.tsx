70144a2c1c36d963126c4d99c27901a50f586b2c
Simon Pallister <simon@pallisters.net>
2025-07-21 13:42:57 +1000

    added config validation

--- src/components/CdpClientWrapper.tsx
+++ src/components/CdpClientWrapper.tsx
@@ -25,6 +25,13 @@ type CdpClientWrapperProps = {
 }
 
 export const CdpClientWrapper = ({ config, children }: CdpClientWrapperProps) => {
+  const isValidConfig = config.writeKey && config.cdpEndpoint
+
+  if (!isValidConfig) {
+    console.warn("CDP: Invalid configuration detected. Tracking disabled.")
+    return <>{children}</>
+  }
+
   return (
     <CdpProvider config={config}>
       <CdpContextProvider>
